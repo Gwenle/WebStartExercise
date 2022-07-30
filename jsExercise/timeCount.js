48a24df105637effe9882f12b4c48d73323ea5be
prxcbf-le <robinwen024@gmail.com>
2022-07-30 17:10:11 +0800

    timecountFinshed

---
+++ jsExercise/timeCount.js
@@ -0,0 +1,31 @@
+const { exit } = require('process');
+
+const readline = require('readline').createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+);
+readline.question('plese input a minute number\n', minute => {
+    let i=1;
+    let countfun=setInterval(()=>{
+        console.log("time remaining "+(minute-i++)+" minute");
+    },60000);
+    setTimeout(() => {
+        clearInterval(countfun);
+        //console.log(minute);
+        const player = require("play-sound")(opts={});
+        // path是音频文件的路径
+        player.play(
+            '../Avicii; Sterling Fox - Talk To Myself.mp3'
+            // "../清枫徐月 - 洛克王国原声带-洛克王国雪人谷（清枫徐月 remix）.mp3"
+        , (err) => {
+            if (err) {
+                //reject();
+                console.log(err);
+            }
+            console.log('play-finished'); 
+            exit(0);
+        });
+    }, minute * 60000)
+});
